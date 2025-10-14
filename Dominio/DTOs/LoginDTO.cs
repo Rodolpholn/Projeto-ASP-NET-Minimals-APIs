b7325031541ea2fd7845acb6f3273e7b41920c3a
Rodolpholn <rodolpho-crack@hotmail.com>
2025-10-14 14:44:01 -0300

    Start Project

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace Projeto_ASP.NET_Minimals_APIs.DtOs;
+// namespace MinimalApi.DtOs;
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
---
+++ Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,6 @@
+namespace Projeto_ASP.NET_Minimals_APIs.Infraestrutura.Db;
+
+public class DbContexto
+{
+    
+} 
